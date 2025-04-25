3b994a2d9a8b050e485d2871717cfe0faa9bb93e
jgrymr <jgrymr@yahoo.com>
2025-04-26 02:42:27 +0800

    Update SubjectScheduleEntry.cs

--- EvaluationSystem/SubjectScheduleEntry.cs
+++ EvaluationSystem/SubjectScheduleEntry.cs
@@ -15,6 +15,66 @@ namespace EvaluationSystem
         public SubjectScheduleEntry()
         {
             InitializeComponent();
+            AMPMComboBox.Items.Add("AM");
+            AMPMComboBox.Items.Add("PM");
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\63915\Documents\Goco.mdf;Integrated Security=True;Connect Timeout=30";
+            SqlConnection myConnection = new SqlConnection(connectionString);
+            
+            String sql = "SELECT * FROM SubjectSchedFile";
+            
+            SqlDataAdapter thisAdapter = new SqlDataAdapter(sql, myConnection);
+            
+            SqlCommandBuilder thisBuilder = new SqlCommandBuilder(thisAdapter);
+            
+            DataSet thisDataSet = new DataSet();
+            thisAdapter.Fill(thisDataSet, "SubjectSchedFile");
+            DataRow thisRow = thisDataSet.Tables["SubjectSchedFile"].NewRow();
+
+            try
+            {
+                if (SubjectEDPCodeTextBox.Text.Equals("") ||
+                    SubjectCodeTextBox.Text.Equals("") ||
+                    DescriptionTextBox.Text.Equals("") ||
+                    TimeStartTextBox.Text.Equals("") ||
+                    TimeEndTextBox.Text.Equals("") ||
+                    DaysTextBox.Text.Equals("") ||
+                    SectionTextBox.Text.Equals("") ||
+                    RoomTextBox.Text.Equals("") ||
+                    SchoolYearTextBox.Text.Equals(""))
+                {
+                    MessageBox.Show("FILL UP THE REMAINING FIELDS");
+                }
+                else
+                {
+                    // Next step: assign values to thisRow
+                    thisRow["SSFEDPCODE"] = SubjectEDPCodeTextBox.Text;
+                    thisRow["SSFSUBJCODE"] = SubjectCodeTextBox.Text;
+                    thisRow["SSFTIMESTART"] = Convert.ToDateTime(TimeStartTextBox.Text);
+                    thisRow["SSFENDTIME"] = Convert.ToDateTime(TimeEndTextBox.Text);
+                    thisRow["SSFDAYS"] = DaysTextBox.Text;
+                    thisRow["SSFSECTION"] = SectionTextBox.Text;
+                    thisRow["SSFROOM"] = RoomTextBox.Text;
+                    thisRow["SSFSCHOOLYEAR"] = Convert.ToInt32(SchoolYearTextBox.Text);
+
+                    thisDataSet.Tables["SubjectSchedFile"].Rows.Add(thisRow);
+                    thisAdapter.Update(thisDataSet, "SubjectSchedFile");
+
+                    MessageBox.Show("Entries Recorded");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
         }
     }
 }
